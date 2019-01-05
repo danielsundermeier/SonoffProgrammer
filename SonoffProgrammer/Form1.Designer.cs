4d2e825b041e09484f7fbe87b1361774a277881b
danielsundermeier <daniel@hof-sundermeier.de>
2019-01-05 15:34:48 +0100

    Initial Commit

---
+++ SonoffProgrammer/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace SonoffProgrammer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cmbPortName = new System.Windows.Forms.ComboBox();
+            this.rtxtDataArea = new System.Windows.Forms.RichTextBox();
+            this.txtSendData = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.ComPort = new System.IO.Ports.SerialPort(this.components);
+            this.cmbFiles = new System.Windows.Forms.ComboBox();
+            this.btnSendFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbPortName
+            // 
+            this.cmbPortName.FormattingEnabled = true;
+            this.cmbPortName.Location = new System.Drawing.Point(12, 12);
+            this.cmbPortName.Name = "cmbPortName";
+            this.cmbPortName.Size = new System.Drawing.Size(166, 21);
+            this.cmbPortName.TabIndex = 2;
+            this.cmbPortName.Text = "Port auswählen";
+            // 
+            // rtxtDataArea
+            // 
+            this.rtxtDataArea.Location = new System.Drawing.Point(12, 44);
+            this.rtxtDataArea.Name = "rtxtDataArea";
+            this.rtxtDataArea.Size = new System.Drawing.Size(820, 399);
+            this.rtxtDataArea.TabIndex = 7;
+            this.rtxtDataArea.Text = "";
+            // 
+            // txtSendData
+            // 
+            this.txtSendData.Location = new System.Drawing.Point(12, 454);
+            this.txtSendData.Name = "txtSendData";
+            this.txtSendData.Size = new System.Drawing.Size(773, 20);
+            this.txtSendData.TabIndex = 8;
+            this.txtSendData.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tb_KeyDown);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(791, 454);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(41, 20);
+            this.btnSend.TabIndex = 9;
+            this.btnSend.Text = "->";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(184, 10);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 10;
+            this.btnConnect.Text = "Verbinden";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // ComPort
+            // 
+            this.ComPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.dataReceived);
+            // 
+            // cmbFiles
+            // 
+            this.cmbFiles.FormattingEnabled = true;
+            this.cmbFiles.Location = new System.Drawing.Point(612, 12);
+            this.cmbFiles.Name = "cmbFiles";
+            this.cmbFiles.Size = new System.Drawing.Size(173, 21);
+            this.cmbFiles.TabIndex = 11;
+            this.cmbFiles.Text = "Config File";
+            // 
+            // btnSendFile
+            // 
+            this.btnSendFile.Enabled = false;
+            this.btnSendFile.Location = new System.Drawing.Point(791, 12);
+            this.btnSendFile.Name = "btnSendFile";
+            this.btnSendFile.Size = new System.Drawing.Size(41, 21);
+            this.btnSendFile.TabIndex = 12;
+            this.btnSendFile.Text = "->";
+            this.btnSendFile.UseVisualStyleBackColor = true;
+            this.btnSendFile.Click += new System.EventHandler(this.btnSendFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 486);
+            this.Controls.Add(this.btnSendFile);
+            this.Controls.Add(this.cmbFiles);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSendData);
+            this.Controls.Add(this.rtxtDataArea);
+            this.Controls.Add(this.cmbPortName);
+            this.Name = "Form1";
+            this.Text = "Sonoff Programmer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbPortName;
+        private System.Windows.Forms.RichTextBox rtxtDataArea;
+        private System.Windows.Forms.TextBox txtSendData;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnConnect;
+        private System.IO.Ports.SerialPort ComPort;
+        private System.Windows.Forms.ComboBox cmbFiles;
+        private System.Windows.Forms.Button btnSendFile;
+    }
+}
+
---
+++ SonoffProgrammer/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SonoffProgrammer
+{
+    public partial class Form1 : Form
+    {
+        private String receivedData = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            rtxtDataArea.VisibleChanged += (sender, e) =>
+            {
+                if (rtxtDataArea.Visible)
+                {
+                    rtxtDataArea.SelectionStart = rtxtDataArea.TextLength;
+                    rtxtDataArea.ScrollToCaret();
+                }
+            };
+
+            updatePorts();
+            UpdateFiles();
+        }
+
+        private void Form1_FormClosing(object sender, EventArgs e)
+        {
+            try
+            {
+                disconnect();
+            }
+            catch(Exception exc)
+            {
+
+            }
+        }
+
+        private void UpdateFiles()
+        {
+            DirectoryInfo dinfo = new DirectoryInfo("./");
+            FileInfo[] Files = dinfo.GetFiles("*.txt");
+            foreach (FileInfo file in Files)
+            {
+                cmbFiles.Items.Add(file.Name);
+            }
+        }
+
+        private void updatePorts()
+        {
+            String[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                cmbPortName.Items.Add(port);
+            }
+        }
+
+        private void Connect()
+        {
+            if (cmbPortName.SelectedIndex != -1)
+            {
+                ComPort.PortName = cmbPortName.Text;
+                ComPort.BaudRate = 115200;
+                ComPort.ReadTimeout = 5000;
+ 
+                try
+                {
+                    ComPort.Open();
+                    txtSendData.Focus();
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(this, "Verbindung zum Comport konnte nicht hergestellt werden!" + exc.Message, "Comport nicht verfügbar", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Bitte alle COM Port Einstellungen setzen", "Serial Port Interface", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+
+            if (ComPort.IsOpen)
+            {
+                btnConnect.Text = "Trennen";
+                btnSend.Enabled = true;
+                btnSendFile.Enabled = true; ;
+                cmbPortName.Enabled = false;
+            }
+        }
+
+        private void disconnect()
+        {
+            ComPort.Close();
+            btnConnect.Text = "Verbinden";
+            btnSend.Enabled = false;
+            btnSendFile.Enabled = false;
+            cmbPortName.Enabled = true;
+        }
+
+        private void sendData(string text)
+        {
+
+            // string recievedData = "";
+            ComPort.Write(text.Trim() + System.Environment.NewLine);
+            rtxtDataArea.AppendText("S: " + text + System.Environment.NewLine);
+            /*
+            rtxtDataArea.AppendText("Warte auf Antwort");
+            int BytesToRead = ComPort.BytesToRead;
+            int runden = 0;
+            int rundenBisAbbruch = 10;
+            while (BytesToRead == 0 & runden < rundenBisAbbruch)
+            {
+                System.Threading.Thread.Sleep(500);
+                rtxtDataArea.AppendText(".");
+                runden++;
+                BytesToRead = ComPort.BytesToRead;
+            }
+            rtxtDataArea.AppendText(System.Environment.NewLine);
+            rtxtDataArea.AppendText("Bytes to Read: " + BytesToRead + System.Environment.NewLine);
+            recievedData = ComPort.ReadExisting();
+            rtxtDataArea.AppendText("R: " + recievedData + System.Environment.NewLine);
+            */
+        }
+
+        private void UpdateTextBox()
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(UpdateTextBox));
+                return;
+            }
+            string data = this.receivedData;
+            this.rtxtDataArea.ForeColor = Color.Green;
+            this.rtxtDataArea.AppendText(data);
+        }
+
+        public void dataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Debug.WriteLine("Daten empfangen");
+            try
+            {
+                SerialPort sp = (SerialPort)sender;
+                this.receivedData = sp.ReadExisting();
+                this.UpdateTextBox();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(this, "Text konnte nicht empfangen werden! " + exc.Message, "Empfangen fehlgeschlagen", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (ComPort.IsOpen)
+            {
+                disconnect();
+            }
+            else
+            {
+                Connect();
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sendData(txtSendData.Text);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(this, "Text konnte nicht gesendet werden! " + exc.Message, "Senden fehlgeschlagen", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        void tb_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Enter)
+            {
+                return;
+            }
+
+            try
+            {
+                sendData(txtSendData.Text);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(this, "Text konnte nicht gesendet werden! " + exc.Message, "Senden fehlgeschlagen", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void btnSendFile_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            string line;
+            string cmd = "Backlog ";
+
+            System.IO.StreamReader file = new System.IO.StreamReader("./" + cmbFiles.Text);
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.Length == 0 || line.Substring(0, 1) == ";")
+                {
+                    continue;
+                }
+
+                Debug.WriteLine(line);
+                cmd += line + ";";
+                counter++;
+            }
+
+            if (counter > 0)
+            {
+                sendData(cmd);
+            }
+                
+            file.Close();
+        }
+    }
+}
